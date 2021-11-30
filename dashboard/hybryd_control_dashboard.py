8d22641202865c6c77fa405c85cfe1ce7d270f05
HybRyd Control <d.walker.14@ucl.ac.uk>
2021-11-30 13:48:12 +0000

    Cleaner instrument reload, also fixing repo

--- dashboard/hybryd_control_dashboard.py
+++ dashboard/hybryd_control_dashboard.py
@@ -3,15 +3,10 @@ from datetime import datetime
 import pyvisa as pv
 from os import path
 import json
-from bokeh.themes import built_in_themes
 from bokeh.io import curdoc
-
-curdoc().theme = "dark_minimal"
-
 
 def timestamp():
     return datetime.now().strftime("%Y-%m-%d@%H:%M:%S")
-
 
 instruments = {}
 rm = pv.ResourceManager()
@@ -30,14 +25,19 @@ except Exception as e:
 
 
 instrument_page = bkm.Panel(title="Instrument Rack",
-                            child=bkm.Div(text="Test 1"))
+                            child=bkl.grid([bkm.Div(text="Test 1")]))
 monitor_page = bkm.Panel(title="Control and Monitor",
-                         child=bkm.Div(text="Test 2"))
-VISA_page = bkm.Panel(title="Connections", child=bkm.Div(text="Test 3"))
+                         child=bkl.grid([bkm.Div(text="Test 2")]))
+VISA_page = bkm.Panel(title="Connections", child=bkl.grid([bkm.Div(text="Test 3")]))
+
 message_buffer = bkl.column(
     [bkm.Div(text=timestamp()), bkm.Div(text="HybRyd Dashboard Startup")],
     sizing_mode="stretch_both")
-messages_page = bkm.Panel(title="System Messages", child=message_buffer)
+message_resolutions = bkl.column([bkm.Div(text=""), bkm.Div(text="")])
+messages_page = bkm.Panel(title="System Messages",
+                          child=bkl.row([message_buffer, message_resolutions])
+                          )
+
 
 page_header = bkm.Div(text="<h1>HybRyd Dashboard</h1>",
                       sizing_mode="stretch_both")
@@ -46,39 +46,53 @@ notifications = bkm.Div(text="", sizing_mode="stretch_both", align="end")
 top_bar = bkl.row([page_header, notifications])
 
 
-def push_message(msg):
+def push_message(msg, resolution_cmd=None):
+    print(msg)
     ts_element = bkm.Div(text=timestamp())
     msg_element = bkm.Div(text=msg)
     message_buffer.children += [ts_element, msg_element]
+    message_resolutions.children += [bkm.Div(text="")]
+    if resolution_cmd is None:
+        message_resolutions.children += [bkm.Div(text="")]
+    else:
+        b = bkm.Button(label = "Resolve")
+        message_resolutions.children += [b]
     if len(message_buffer.children) > app_config["retained_messages"]:
         message_buffer.children = message_buffer.children[-app_config["retained_messages"]:]
+        message_resolutions.children = message_resolutions.children[-app_config["retained_messages"]:]
+
     notifications.update(text="New System Message(s)")
 
 
 def refresh_instruments():
     ports = rm.list_resources()
+    visa_ports_display.children = []
     if ports:
         for port in ports:
             try:
                 candidate = rm.open_resource(port)
+
                 idn = candidate.query("*IDN?")
-                idn.replace("*IDN", "").strip()
-                instruments[port] = Instrument(
-                    candidate, port, *(idn.split(",")))
+                idn = idn.replace("*", "").replace("IDN", "").strip()
+
+                instruments.update({port: Instrument(
+                    candidate, port, *(idn.split(",")[:4]))})
+
+                visa_ports_display.children += [bkm.Div(text=port+"\t:\t"+str(instruments[port]))]
+
             except Exception as e:
                 push_message(str(e))
+                visa_ports_display.children += [bkm.Div(text=port+"\t:\t"+str(e))]
     else:
         message = "No instrument ports were found. Check power and data connections?"
         push_message(message)
 
-    for port in ports:
-        instruments[port].load_config()
-
-
 visa_refresh_button = bkm.Button(label="Load Instruments", width=200)
 visa_refresh_button.on_click(refresh_instruments)
-
-VISA_page.update(child=visa_refresh_button)
+visa_ports_display = bkl.column([bkm.Div(text="", width=1200)], sizing_mode="stretch_both")
+VISA_page.update(
+    child = bkl.column([visa_refresh_button, visa_ports_display])
+    )
 
 
 class Instrument:
@@ -86,17 +100,14 @@ class Instrument:
     HYbRyd control dashboard, driven by prebuilt config files."""
 
     def __init__(
-        self, device, port, manufacturer, model,
-        serial="Unspecified", firmware="Unspecified"
+        self, device, port, *spec
     ):
 
         ### THE FOLLOWING MEMBER VARIABLES ARE SET AT INSTANTIATION ###
 
         self.device = device  # The open VISA instrument
         self.port = port  # The port identifier the instrument is located at
-        self.name = manufacturer + " " + model  # The name of the instrument
-        self.serial = serial  # Instrument serial number
-        self.firmware = firmware  # Instrument firmware revision
+        self.spec = spec  # The identity of the instrument
 
         ### THE REMAINING MEMBER VARIABLES ARE SET FROM A CONFIG FILE ###
 
@@ -120,7 +131,7 @@ class Instrument:
         # This location should be relative to the installation directory of
         # the HybRyd Control Dashboard
         dst = path.abspath(path.join(
-            ".", self.manufacturer, self.model, "instrument.cfg"))
+            ".","drivers", *self.spec, "instrument.cfg"))
 
         # Check that the file exists!
         self.config_found = path.isfile(dst)
@@ -136,7 +147,7 @@ class Instrument:
                 self.desc = config['desc']
                 self.man = config['manual']
                 self.settable = config['settable']
-                self.readable = config['readable']
+                self.gettable = config['gettable']
 
             # If we cannot load the file and assign the variables of the Instrument
             except Exception as e:
@@ -146,13 +157,16 @@ class Instrument:
                 # And add the exception to the message buffer
                 push_message(str(e))
         else:
-            push_message(dst + "was not found.")
+            push_message(dst + " was not found.")
 
         # Report the success state of the config load at end of method call
         return self.config_found
 
     def gen_rack_element(self):
         pass
+
+    def __str__(self):
+        return "_".join(self.spec)
 
 
 t = bkm.Tabs(tabs=[instrument_page, monitor_page, VISA_page,
