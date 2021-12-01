b4aafae4c38e5c6386be0b8a15c6fa9d6843e468
Luke Brown <luke.brown.19@ucl.ac.uk>
2021-12-01 14:33:37 +0000

    dummy instrument page

--- dashboard/pages/page_dummy.py
+++ dashboard/pages/page_dummy.py
@@ -1,37 +1,31 @@
 import pyvisa as pv
 from bokeh import models as bkm, layouts as bkl
+import numpy as np
 
 from .page_system_messages import log
 from instrument import Instrument
+from utils import insturment_manager
 
 rm = pv.ResourceManager()
 
-page = bkm.Panel(title="Dummy Instrument",
-                            child=bkl.grid([bkm.Div(text="Test 1")]))
+page = bkm.Panel(title="Dummy Instrument")
 
 def refresh_instruments():
-    ports = rm.list_resources()
     visa_ports_display.children = []
-    if ports:
-        for port in ports:
-            try:
-                candidate = rm.open_resource(port)
+    
+    class dummy_inst:
+        def query(self, command):
+            return np.random.lognormal(1, 0.1, 1)
+        
+        def write(self, command):
+            return
+   
+    candidate = dummy_inst()
+    port = '0'
+    idn = 'DUMMYINST'
 
-                idn = candidate.query("*IDN?")
-                idn = idn.replace("*", "").replace("IDN", "").strip()
-
-                instruments.update({port: Instrument(
-                    candidate, port, *(idn.split(",")[:4]))})
-
-                visa_ports_display.children += [bkm.Div(text=port+"\t:\t"+str(instruments[port]))]
-
-            except Exception as e:
-                log(str(e))
-                visa_ports_display.children += [bkm.Div(text=port+"\t:\t"+str(e))]
-    else:
-        message = "No instrument ports were found. Check power and data connections?"
-        log(message)
-
+    insturment_manager.update({port: Instrument(candidate, port, *idn)})
+    visa_ports_display.children += [bkm.Div(text=port+"\t:\t"+str(insturment_manager[port]))]
 
 
 visa_refresh_button = bkm.Button(label="Load Instruments", width=200)
